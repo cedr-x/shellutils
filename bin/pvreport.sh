2da304d1f1ed692fb10bd6affb31380ed8b62f3a
Cedric de l'Epine <cegithub@delepine.org>
2016-11-24 11:53:35 +0100

    rename lvreport.sh to pvreport.sh which is more appropriate

--- bin/pvreport.sh
+++ bin/pvreport.sh
@@ -2,7 +2,7 @@
 # Enumerate all physical volumes and show what's the usage on each
 
 for I in $( pvs -o pv_name --noheadings  ) ; do 
-	echo $I \($(lsblk ${I/[0-9]/}  -d  -o MODEL,REV,SIZE -n )\)
+	echo $I \($(lsblk ${I/[0-9]/}  -d  -o MODEL,REV,SIZE -n )\) $( pvs $I  --noheadings --nosuffix --units g  -o pv_size,pv_used,pv_free )
 	pvs $I --all  --noheadings --segment --nosuffix --units g -o seg_size,vg_name,lv_name | sed 's/_[^ ]*//;s/\[//;s/ *$//' \
 	| awk '
 		$3=="" {
@@ -16,6 +16,7 @@ for I in $( pvs -o pv_name --noheadings  ) ; do
 		}  
 		END { 
 			for (item in QTE) 
-				printf("\t%s/%s\t%9i\t%4.1f%\n",vg,item, QTE[item], 100*QTE[item]/tot) 
+				printf("\t%-20s  %12i  %4.1f%\n",vg"/"item, QTE[item], 100*QTE[item]/tot) 
 		} '|sort 
+	echo
 done
